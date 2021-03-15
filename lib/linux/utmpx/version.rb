b461398be9a8187dce36738a67936f93d94592f5
Kentaro Hayashi <kenhys@gmail.com>
2021-03-15 10:05:09 +0900

    Import bundle gem template files

---
+++ lib/linux/utmpx/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Linux
+  module Utmpx
+    VERSION = "0.1.0"
+  end
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "linux/utmpx"
+
+require "test-unit"
