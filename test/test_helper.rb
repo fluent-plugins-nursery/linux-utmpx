b8d82c1c022c2341832efa26e98c5ad0dd128b7d
Kentaro Hayashi <hayashi@clear-code.com>
2021-03-23 17:10:25 +0900

    Add helper method to specify .dump

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,4 +3,11 @@
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "linux/utmpx"
 
+require "time"
 require "test-unit"
+
+def dump_fixture_path(name)
+  File.join(File.dirname(__FILE__),
+            "fixtures",
+            "#{name}.dump")
+end
